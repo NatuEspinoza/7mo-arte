1fb982ba19ae0d9cc62e2d60bfff414fc32e5a31
NatuEspinoza <natalia.utemdcv@gmail.com>
2018-01-10 10:39:29 -0300

    Realizando README

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,59 @@
 $(document).ready(function() {
+  /* llamo al id del button y lo guardo en una variable */
+  var searchBtn = $('#searchBtn');
+  /* llamo al id del input y lo guardo en una variable */
+  var titleFilm = $('#title');
+
+/* creo el evento de click en el boton */
+  searchBtn.click(function searchMovie(){
+    /* creo una variable que guardara los datos del input */
+    var titleText = titleFilm.val();
+    //alert('searchMovie ' + titleText);
+    /* creo variable en donde guarda la informacion de la data */
+    /* que en este caso seria los titulos de peliculas */
+    var url = 'http://www.omdbapi.com/?apikey=3a181f1c&s=' + titleText;
+    console.log(url);
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: renderMovies,
+      error: renderError
+    })
+  })
+  function renderMovies (response){
+    //console.log(response);
+    var movies = response.Search;
+    var resultsUl = $('#results');
+    resultsUl.empty();
+
+    for (var i in movies) {
+      var movie = movies[i];
+      var title = movie.Title;
+      var imdbID = movie.imdbID;
+      var poster = movie.Poster;
+
+      console.log([title, imdbID, poster]);
+
+      var liMovie = $('<li class="list-group-item"></li>');
+      var posterImg = $('<img src="' + poster + '" width="50px"/>');
+      liMovie.append(posterImg);
+      liMovie.append(title);
+      resultsUl.append(liMovie);
+      liMovie.click(function renderDetails(){
+        console.log('render');
+      });
+    }
+
+  }
+  function renderError (error){
+    console.log(error);
+  }
+})
+
+
+
+
+/*
   $.ajax({
     type: "GET",
     url: "http://www.omdbapi.com/?apikey=3a181f1c&s=type",
@@ -7,7 +62,7 @@ $(document).ready(function() {
 
         console.log(data);
 
-
+*/
 /*
       $("ul").children().remove();
         $(data).find("MenuCompleto").each( function() {
@@ -17,8 +72,7 @@ $(document).ready(function() {
     }
   });
 */
-};
-});
+
 
 /**
  * Función para Carrusel
@@ -48,4 +102,4 @@ $(document).ready(function() {
   * Fin de Función para Carrusel
   */
 
-});
+
